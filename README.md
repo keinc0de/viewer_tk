610d2d68843158fe621dfd86da4f8d9452ecdb2c
hector martinez <keincode@gmail.com>
2024-05-08 17:30:42 -0400

    cambiar de FIT a PAN con espacio - cambiado event.width a widget.winfo_width

--- README.md
+++ README.md
@@ -8,7 +8,13 @@ visor de imagen con tk y Canvas (para usar como widgtet)
 ![](para_md/viewer%20tk-canvas%20FIT.gif)
 ![](para_md/viewer%20tk-canvas%20PAD.gif)
 
+<img src="para_md/viewer%20tk-canvas%20FIT.gif", width=50% height=50%>
+
+
 AJUSTAR
+
 `visor.AJUSTAR = True` Para usar FIT mostrar imagen centrada
+
 `visor.AJUSTAR = False` Para PAD mostrar imagen manteniendo la escala
+
 
--- viewer_tk.py
+++ viewer_tk.py
@@ -1,27 +1,33 @@
 import tkinter as tk
 from pathlib import Path
 from PIL import ImageTk, Image, ImageOps
+from mi_checkbox import MiCheckbox
 
 
 class ViewerTk(tk.Frame):
     """visor de imagenes basico (no gifs)
+    Viewer().AJUSTAR: (por defecto False)
+        True: mostrar imagen centrada
+        False: mostrar imagen manteniendo escala
     """
-    def __init__(self, parent, bg="gray"):
-        super(ViewerTk, self).__init__(master=parent, bg=bg)
+    def __init__(self, parent, bg="gray", **kw):
+        super(ViewerTk, self).__init__(master=parent, bg=bg, **kw)
         self.parent = parent
         self.bg = bg
         self.IMG = None
         self.AJUSTAR = False
-        self._config_viewer()
+        self._config_viewertk()
         
-    def _config_viewer(self):
-        self.cv = tk.Canvas(master=self.parent, bg='red', borderwidth=0, bd=0)
+    def _config_viewertk(self):
+        self.cv = tk.Canvas(master=self.parent, bg=self.bg, highlightthickness=0)
         self.cv.grid(row=0, column=0, sticky='wens')
-        self.bind("<Configure>", self.resizer)
+        self.bind("<Configure>", self._resizer)
         self.parent.rowconfigure(0, weight=1)
         self.parent.columnconfigure(0, weight=1)
+        self.parent.bind("<space>", self.cambia)
 
     def asigna_imagen(self, archivo):
+        """archivo: ruta de la imagen"""
         img = Path(archivo).as_posix()
         img_tk = ImageTk.PhotoImage(Image.open(img))
         w = img_tk.width()
@@ -33,22 +39,30 @@ class ViewerTk(tk.Frame):
         }
         self.img_id = self.cv.create_image(0,0,image=img_tk, anchor='nw')
 
-    def resizer(self, e):
-        # if self.IMG and "imagen" in self.IMG.keys() and hasattr(self.img_id, "self"):
+    def _resizer(self, e=None):
         if self.IMG and "imagen" in self.IMG.keys():
-            medidas = (e.width, e.height)
+            # medidas = (e.width, e.height)
+            self.parent.update()
+            medidas = (self.parent.winfo_width(), self.parent.winfo_height())
             img = Image.open(self.IMG.get('imagen'))
-            # red_img = img.resize((e.width, e.height), Image.Resampling.BILINEAR)
-            if self.AJUSTAR:
-                # red_img = ImageOps.fit(img, medidas, method=5, centering=(1.0, 0.0)) # anclado arriba
-                red_img = ImageOps.fit(img, medidas, method=5)
-            else:
 
-                red_img = ImageOps.pad(img, medidas, method=5, color=self.bg)
-            img_tk = ImageTk.PhotoImage(red_img)
+            d = {'size':medidas, 'method':5, 'image':img}
+            rimg = ImageOps.fit(**d) if self.AJUSTAR else ImageOps.pad(color=self.bg, **d)
+            img_tk = ImageTk.PhotoImage(rimg)
             self.IMG["img_tk"] = img_tk
             self.cv.itemconfigure(self.img_id, image=img_tk)
-        
+
+    def cambia(self, e=None):
+        self.AJUSTAR = False if self.AJUSTAR else True
+        self._resizer()
+
+    def __str__(self):
+        s = """ViewerTk
+        METODOS:
+            asigna_imagen(archivo)
+        """
+        return s
+    
 
 if __name__=="__main__":
     app = tk.Tk()
@@ -56,8 +70,10 @@ if __name__=="__main__":
 
     visor = ViewerTk(app, "black")
     visor.grid(row=0, column=0, sticky='wens')
-    visor.AJUSTAR = True
+    # visor.AJUSTAR = True
     visor.asigna_imagen("otros/alic.jpg")
-
+    # print(visor.__doc__)
+    # help(visor)
+    print(visor)
     
     app.mainloop()
